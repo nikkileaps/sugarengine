238ecc831f0640d475ee6ff35718ce18049851c8
nikkileaps <nikki@foxleapmoon.com>
2026-02-02 15:19:30 -0700

    [feat] have deploy script package up existing home page in rackwickcity repo and deploy with game

--- scripts/deploy-to-rackwick.sh
+++ scripts/deploy-to-rackwick.sh
@@ -2,23 +2,30 @@
 set -e
 
 DIST_DIR="dist-game"
+RACKWICK_DIR="$HOME/projects/rackwickcity"
+DEPLOY_DIR="$RACKWICK_DIR/dist"
 
 echo "Building game for deploy..."
 DEPLOY_BUILD=true npm run game:build
 
+echo "Preparing deploy directory..."
+cd "$RACKWICK_DIR"
+
+# Create clean deploy directory
+rm -rf "$DEPLOY_DIR"
+mkdir -p "$DEPLOY_DIR"
+
+# Copy landing page (pubsite) to root
+cp -r pubsite/* "$DEPLOY_DIR/"
+
+# Copy game build to /game subdirectory
+mkdir -p "$DEPLOY_DIR/game"
+cp -r "$OLDPWD/$DIST_DIR"/* "$DEPLOY_DIR/game/"
+
 echo "Deploying to Netlify..."
-
-# Deploy directly to Netlify (to the /game subdirectory)
-# First time: run 'netlify login' and 'netlify link' in rackwickcity repo
-cd "$HOME/projects/rackwickcity"
-
-# Copy game build to game/ subdirectory
-rm -rf game/
-mkdir -p game/
-cp -r "$OLDPWD/$DIST_DIR"/* game/
-
-# Deploy the whole site
-netlify deploy --prod --dir=.
+netlify deploy --prod --dir="$DEPLOY_DIR"
 
 echo ""
 echo "Deployed!"
+echo "  Landing page: https://rackwickcity.com"
+echo "  Game: https://rackwickcity.com/game"
